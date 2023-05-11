7d110394fe623550ca6ab06026447ba7fb8bb162
Miguel Ribeiro <70304654+miguel-ribeiro-07@users.noreply.github.com>
2023-05-10 22:21:38 -0300

    Correção erros p/ rodar

--- ws/index.js
+++ ws/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express ()
 const morgan = require('morgan');
-//const busboy = require('connect-busboy');
-//const busboyBodyParser = require('busboy-body-parser')
 
 require('./database');
 
@@ -11,8 +9,6 @@ require('./database');
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(cors())
-//app.use(busboy())
-//app.use(busboyBodyParser())
 
 //VARIABLES
 app.set('port', 8000);
