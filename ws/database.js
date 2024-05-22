68842c99d83a12995c7c2ff050b1fc10c40b6208
Miguel Ribeiro <70304654+miguel-ribeiro-07@users.noreply.github.com>
2024-05-22 16:00:04 -0300

    Comments future improvements

--- ws/database.js
+++ ws/database.js
@@ -1,3 +1,5 @@
+// need .env
+
 const mongoose = require('mongoose');
 const URI = 'mongodb+srv://spacodabelezaUser:5HAmVaH622QHtSrv@clusterdev.urszlpv.mongodb.net/spaco-da-beleza?retryWrites=true&w=majority';
 
