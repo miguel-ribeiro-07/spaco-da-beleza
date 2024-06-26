c7cc19435930bff53a6e25d917cbd46beb642d9f
Miguel Ribeiro <miguel.ribeiro.07.contato@gmail.com>
2024-06-26 10:14:00 -0300

    Add .env

--- ws/database.js
+++ ws/database.js
@@ -3,4 +3,4 @@
 const mongoose = require('mongoose');
 const URI = 'mongodb+srv://spacodabelezaUser:5HAmVaH622QHtSrv@clusterdev.urszlpv.mongodb.net/spaco-da-beleza?retryWrites=true&w=majority';
 
-mongoose.connect(URI).then(() => console.log('DB is online')).catch(() => console.log(err));
+mongoose.connect(URI).then(() => console.log('MongoDB is online')).catch(() => console.log(err));
